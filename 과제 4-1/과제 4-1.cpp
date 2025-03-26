2d44280a5a6efd36f71b993f301245aee2b63436
hjshim0304 <hjshim0304@naver.com>
2025-03-26 15:19:40 +0900

    과제 4-1 추가

---
+++ 과제 4-1/과제 4-1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person {
+	string name;
+	string tel;
+public:
+	Person(){}
+	Person(string n, string t) {
+		name = n;
+		tel = t;
+	};
+	~Person() {
+
+	};
+
+	string getName() {
+		return name;
+	}
+	string getTel() {
+		return tel;
+	}
+
+	void set(string n, string t) {
+		name = n;
+		tel = t;
+	}
+};
+
+int main() {
+	Person list[3];
+	char nameStr[30];
+	char telStr[50];
+
+	cout << "이름과 전화 번호를 입력해 주세요" << endl;
+	for (int i = 0; i < 3; i++) {
+		char input[50];
+		cout << "사람" << i + 1 << ">> ";
+		cin >> nameStr;
+		cin >> telStr;
+		list[i].set(nameStr, telStr);
+	}
+
+	cout << "모든 사람의 이름은 ";
+	for (int i = 0; i < 3; i++){
+		cout << list[i].getName() << " ";
+	}
+	cout << endl;
+
+	char finding[30];
+	cout << "전화번호 검색합니다. 이름을 입력하세요>> ";
+	cin >> finding;
+	for (int i = 0; i < 3; i++) {
+		if (list[i].getName() == finding) {
+			cout << "전화번호는 " << list[i].getTel() << endl;
+		}
+	}
+}
