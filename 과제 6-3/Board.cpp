150a2c2884dd8639762cfe6cf6dd0ae32f05d4c4
IT308 <IT308@DESKTOP-2LCMQUP>
2025-04-14 14:46:53 +0900

    6-3 수정

--- 과제 6-3/Board.cpp
+++ 과제 6-3/Board.cpp
@@ -5,13 +5,14 @@
 using namespace std;
 
 int Board::size = 0;
+string Board::text[100];
 
 void Board::add(string msg){
 	text[size] = msg;
 	size++;
 }
 void Board::print(){
-	cout << "게시판입니다." << endl;
+	cout << "************* 게시판입니다. *************" << endl;
 	for (int i = 0; i < size; i++) {
 		cout << i << ": " << text[i] << endl;
 	}
--- 과제 6-3/Board.h
+++ 과제 6-3/Board.h
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 class Board {
 private:
 	Board() { size = 0; };
