45f11650e447cae211d5a78d919ec91a4b7867a4
IT308 <IT308@DESKTOP-2LCMQUP>
2025-04-14 14:39:23 +0900

    통합

---
+++ 과제 6-2(2)/과제 6-2(2).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int big(int x, int y, int z = 100) {
+	int bigger;
+	if (x > y) bigger = x;
+	else if (x < y) bigger = y;
+	if (bigger > z) return z;
+	else return bigger;
+}
+
+int main() {
+	int x = big(3, 5); // 3과 5중 큰 값 5는 최대값 100보다 작으므로, 5 리턴
+	int y = big(300, 60); // 300과 60중 큰 값 300이 최대값 100보다 크므로, 100 리턴
+	int z = big(30, 60, 50); // 30과 60 중 큰 값 60이 최대값 50보다 크므로, 50 리턴
+
+	cout << x << ' ' << y << ' ' << z << endl;
+}
