8b5ef2b6898823225773719fc511378c9b8fb1ff
IT308 <IT308@DESKTOP-2LCMQUP>
2025-04-14 14:35:08 +0900

    통합

--- 과제 6-3/Board.h
+++ 과제 6-3/Board.h
@@ -1,9 +1,12 @@
+#include <iostream>
+#include <string>
+
 class Board {
 private:
 	Board() { size = 0; };
 public:
-	static int size;            // 게시판에 저장할 위치 정보 값 
-	static string text[100];    // 최대 100개의 게시글 저장 공간
+	static int size;            // 게시판에 저장할 위치 정보 값
+	static string text[100];	// 최대 100개의 게시글 저장 공간
 	static void add(string text);
 	static void print();
 };
